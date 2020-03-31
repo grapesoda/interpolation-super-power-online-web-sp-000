fc44b69c615fef76cbf9430684f1259388e9d52b
Tres Tarwater <trest03@gmail.com>
2020-03-31 18:06:26 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,5 @@
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts 
+def display_rainbow
+  puts display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+end
+
+display_rainbow
