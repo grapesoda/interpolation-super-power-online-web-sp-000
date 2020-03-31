c3b33fbbf92381087d62c177794dfadda95ecd49
Tres Tarwater <trest03@gmail.com>
2020-03-31 19:14:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,12 +1,2 @@
-def display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
-R = "red"
-O = "orange"
-Y = "yellow"
-G = "green"
-B = "blue"
-I = "indigo"
-V = "violet"
-  puts "#{R[0], O[1], Y[2], G[3], B[4], I[5], V[6]}"
-end
-
-display_rainbow(colors)
+def display_rainbow(colors)
+  puts "R: #{color[1]}, O: #{color[1]}, #{color[2]}"
