d323f5b06f589f31ff759c0c4ae90afecbdc82cb
Tres Tarwater <trest03@gmail.com>
2020-03-31 19:04:39 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
-def display_rainbow(colors)
-  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+def display_rainbow(colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
 R = 'red'
 O = 'orange'
 Y = 'yellow'
